f6fc2e39da6d8fecb59c3cb8c4578301e73667c5
Mrmatrixx77 <kunalsk2001@gmail.com>
2024-06-07 16:17:54 +0530

    auth routes

--- backend/index.ts
+++ backend/index.ts
@@ -1,30 +1,30 @@
-import express,{Request,Response} from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import USerRoutes from "./src/routes/user.route";
+import authRoutes from "./src/routes/auth.route";
 import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
-mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING as string).then(()=>{
-    console.log("DB Connected")
-})
+mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING as string).then(() => {
+  console.log("DB Connected");
+});
 
-
-app.get("/api/test" , async (req : Request,res : Response)=>{
-    res.json({message : "api working fine"});
-
-})
+app.get("/api/test", async (req: Request, res: Response) => {
+  res.json({ message: "api working fine" });
+});
 
 // user routes
+app.use("/api/users", USerRoutes);
+//auth routes
+app.use("/api/auth", authRoutes);
 
-app.use("/api/users",USerRoutes);
-
-const PORT : number = 8080;
-app.listen(PORT,()=>{
-    console.log(`app is running on ${PORT}`);
-})
+const PORT: number = 8080;
+app.listen(PORT, () => {
+  console.log(`app is running on ${PORT}`);
+});
