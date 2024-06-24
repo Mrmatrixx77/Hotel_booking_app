e887d2d0447fafc0854381c1394cb752afaf7003
Mrmatrixx77 <kunalsk2001@gmail.com>
2024-06-24 11:24:35 +0530

    update

--- backend/index.ts
+++ backend/index.ts
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import USerRoutes from "./src/routes/user.route";
 import authRoutes from "./src/routes/auth.route";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 const app = express();
 app.use(express.json());
@@ -16,18 +17,26 @@ app.use(
   })
 );
 app.use(cookieParser());
-mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING as string).then(() => {
-  console.log("DB Connected :",process.env.MONGO_DB_CONNECTION_STRING);
-});
+mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING as string);
 
 app.get("/api/test", async (req: Request, res: Response) => {
   res.json({ message: "api working fine" });
 });
+const distPath = path.join(__dirname, "../..", "frontend", "dist");
+const indexPath = path.join(distPath, "index.html");
+
+
+console.log("distPath:", distPath);
+console.log("indexPath:", indexPath);
+app.use(express.static(path.resolve(__dirname, "../../frontend/dist")));
 
 // user routes
 app.use("/api/users", USerRoutes);
 //auth routes
 app.use("/api/auth", authRoutes);
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.resolve(__dirname, indexPath));
+});
 
 const PORT: number = 8080;
 app.listen(PORT, () => {
